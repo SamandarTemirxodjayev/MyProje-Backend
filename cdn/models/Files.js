71ee96d481a536a48df5e1413cc2af75f43f0152
Samandar <iwtuz166@gmail.com>
2024-11-23 13:17:04 +0500

    update cdn for new version

--- cdn/models/Files.js
+++ cdn/models/Files.js
@@ -7,27 +7,31 @@ const filesSchema = new mongoose.Schema(
 			type: Number,
 		},
 		file_name: {
-			type: String,
+			type: String, // Original uploaded file name
 		},
 		file_id: {
-			type: mongoose.Types.ObjectId,
+			type: mongoose.Types.ObjectId, // Unique identifier for the file
 		},
-		file_url: {
-			type: String,
+		file_urls: {
+			type: Object,
+			required: true,
 		},
+
 		admin_id: {
 			type: Number,
-			ref: "admins",
+			ref: "admins", // Reference to the admins collection
 		},
 		createdAt: {
 			type: Number,
-			default: Date.now(),
+			default: Date.now(), // Automatically set the creation timestamp
 		},
 	},
 	{
-		versionKey: false,
+		versionKey: false, // Disable version key (__v field)
 	},
 );
+
+// Apply AutoIncrement plugin for the `_id` field
 filesSchema.plugin(AutoIncrement, {modelName: "file", fieldName: "_id"});
 
 const Files = mongoose.model("files", filesSchema);
