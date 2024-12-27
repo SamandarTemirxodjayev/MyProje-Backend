fd9024610df28e5022e71d0dca15e6e8dbe26832
Samandar <iwtuz166@gmail.com>
2024-12-27 17:56:51 +0500

    modify env and pagination

--- utils/helpers.js
+++ utils/helpers.js
@@ -154,21 +154,21 @@ exports.paginate = (
 			totalPages,
 		},
 		_links: {
-			self: `${process.env.SITE_URL}${baseUrl}${path}?page=${page}&limit=${limit}${additionalParams}`,
+			self: `${process.env.SITE_URLS}${baseUrl}${path}?page=${page}&limit=${limit}${additionalParams}`,
 			next:
 				page < totalPages
-					? `${process.env.SITE_URL}${baseUrl}${path}?page=${
+					? `${process.env.SITE_URLS}${baseUrl}${path}?page=${
 							page + 1
 					  }&limit=${limit}${additionalParams}`
 					: null,
 			prev:
 				page > 1
-					? `${process.env.SITE_URL}${baseUrl}${path}?page=${
+					? `${process.env.SITE_URLS}${baseUrl}${path}?page=${
 							page - 1
 					  }&limit=${limit}${additionalParams}`
 					: null,
-			first: `${process.env.SITE_URL}${baseUrl}${path}?page=1&limit=${limit}${additionalParams}`,
-			last: `${process.env.SITE_URL}${baseUrl}${path}?page=${totalPages}&limit=${limit}${additionalParams}`,
+			first: `${process.env.SITE_URLS}${baseUrl}${path}?page=1&limit=${limit}${additionalParams}`,
+			last: `${process.env.SITE_URLS}${baseUrl}${path}?page=${totalPages}&limit=${limit}${additionalParams}`,
 		},
 	};
 };
